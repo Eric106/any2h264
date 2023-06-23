627e86a0925c0f949a6a62b3e90a2a3afbf4af7e
Eric106 <ercv106@hotmail.com>
2023-06-23 09:45:00 -0600

    Fri 23 Jun 2023 09:45:00 AM CST

--- any2h264.py
+++ any2h264.py
@@ -1,8 +1,9 @@
 from os import listdir, system, mkdir
-from os.path import exists
+from os.path import exists, abspath
 from argparse import ArgumentParser
 from dataclasses import dataclass, field
 from platform import platform
+import sys
 
 @dataclass(frozen=False)
 class Transcoder:
@@ -16,6 +17,8 @@ class Transcoder:
 
     def __post_init__(self) -> list[str]:
         self.os_type = platform()
+        self.input_folder = abspath(self.input_folder)
+        self.output_folder = abspath(self.output_folder)
         if self.ffmpeg_path == None:
             self.ffmpeg_path = '/usr/bin/ffmpeg' if "Linux" in self.os_type else "ffmpeg"
         video_extensions = ['avi','mp4','mkv']
@@ -37,13 +40,16 @@ class Transcoder:
             system(ffmpeg_command)
 
 def main():
-    parser = ArgumentParser(description="any2h264 args parser")
-    parser.add_argument('-i', dest='input_folder', type=str)
-    parser.add_argument('-o', dest='output_folder', type=str)
-    parser.add_argument('-ffmpeg', dest='ffmpeg_path', type=str)
-    args = parser.parse_args()
-    transcoder = Transcoder(args.input_folder, args.output_folder, args.ffmpeg_path)
-    transcoder.transcode_to_h264()
+    try:
+        parser = ArgumentParser(description="any2h264 args parser")
+        parser.add_argument('-i', dest='input_folder', type=str)
+        parser.add_argument('-o', dest='output_folder', type=str)
+        parser.add_argument('-ffmpeg', dest='ffmpeg_path', type=str)
+        args = parser.parse_args()
+        transcoder = Transcoder(args.input_folder, args.output_folder, args.ffmpeg_path)
+        transcoder.transcode_to_h264()
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == '__main__':
     main()
