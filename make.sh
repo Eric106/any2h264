f7af0008ad1e652f31dd1c59f42f18ed9431ae47
Eric106 <ercv106@hotmail.com>
2024-07-26 08:17:23 -0600

    Fri 26 Jul 2024 08:17:23 AM CST

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 pyinstaller --onefile any2h264.py -y
-sudo cp dist/any2h264 /usr/bin/
+sys_arch=$(arch)
+mv dist/any2h264 dist/any2h264_$sys_arch
+sudo cp dist/any2h264_$sys_arch /usr/bin/any2h264
 sudo chmod +x /usr/bin/any2h264
